4b43a2e8ac3055efa482525baeb6603cceb9cc9a
Wen Ting Wang <chubberlisk@outlook.com>
2020-05-31 15:37:17 +0100

    Fix incorrect components path in Tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  purge: ["./components/**/*.js", "./pages/**/*.js"],
+  purge: ["./src/components/**/*.js", "./pages/**/*.js"],
   theme: {
     backgroundColor: (theme) => ({
       ...theme("colors"),
