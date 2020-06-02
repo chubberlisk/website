64164351914c81a9d261b43524c01174ad625f93
Wen Ting Wang <chubberlisk@outlook.com>
2020-06-02 20:32:13 +0100

    Add core and custom components directory

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,25 +1,10 @@
 import React from "react";
-import Layout from "../components/Layout";
-import Section from "../components/Section";
+import Layout from "../components/core/Layout";
+import LandingSection from "../components/custom/LandingSection";
 
 const Home = () => (
   <Layout>
-    <Section id="landing" className="text-center">
-      <div className="container">
-        <h1>
-          Hi there!
-          <br />
-          My name is <b>Wen Ting Wang</b>.
-          <br />
-          But you can just call me
-          <br />
-          <mark className="bg-white">
-            <b>Ting</b>
-          </mark>
-          .
-        </h1>
-      </div>
-    </Section>
+    <LandingSection />
   </Layout>
 );
 
