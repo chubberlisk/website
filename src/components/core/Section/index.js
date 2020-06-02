2f79d2ea9e383f6afceb4c2e54d562c5c580eaba
Wen Ting Wang <chubberlisk@outlook.com>
2020-06-02 20:50:28 +0100

    Add height and padding to Section component

--- src/components/core/Section/index.js
+++ src/components/core/Section/index.js
@@ -5,12 +5,13 @@ import styles from "./index.module.css";
 const Section = ({
   id,
   backgroundColor = "bg-wtw-yellow",
+  height = "h-screen",
   className,
   children,
 }) => (
   <section
     id={id}
-    className={classNames(styles.section, backgroundColor, className)}
+    className={classNames(styles.section, backgroundColor, height, className)}
   >
     {children}
   </section>
