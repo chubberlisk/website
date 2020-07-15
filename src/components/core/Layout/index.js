ccd3e0b9b503d613b6a0a38652ea772818e1f94e
Wen Ting Wang <chubberlisk@outlook.com>
2020-07-15 17:42:04 +0100

    Add links to icons in Layout

--- src/components/core/Layout/index.js
+++ src/components/core/Layout/index.js
@@ -10,6 +10,36 @@ const Layout = ({ title = "Wen Ting Wang", children }) => (
         name="description"
         content="Wen Ting Wang, software engineer at Made Tech, based in London, UK."
       ></meta>
+      <link rel="icon" href="favicon.ico" />
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="apple-touch-icon.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="512x512"
+        href="android-chrome-512x512.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="192x192"
+        href="android-chrome-192x192.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="favicon-16x16.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="favicon-32x32.png"
+      />
     </Head>
     <main>{children}</main>
     <Footer />
