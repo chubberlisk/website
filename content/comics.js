e38b471992d3d62ef4b9906a99cb0c0238a26f9d
Wen Ting Wang <chubberlisk@users.noreply.github.com>
2021-11-20 21:38:15 +0000

    Add more comics

--- content/comics.js
+++ content/comics.js
@@ -1,10 +1,64 @@
 export default [
   {
-    title: "I am Tingker Bell",
-    publishDate: "2021-01-09",
+    title: "It actually doesn't make sense",
+    publishDate: "2021-01-09T23:02:00",
     image: {
-      src: "/images/paintings/1-i-am-tingker-bell.png",
-      alt: "4 panel comic, with each panel of Tingker Bell",
+      src: "/images/paintings/it-actually-doesnt-make-sense.png",
+      alt: [
+        "4 panel comic.",
+        "In the top left panel, Paint Ting and Tingker Bell are standing next to each other.",
+        "Paint Ting has a paint brush against Tingker Bell's face.",
+        "In the top right panel, Paint Ting has small ducks around over their head and scratching their head in confusion.",
+        "Paint Ting says 'Y'know, it actually doesn't make sense for you to have both wings on one side when you're not flying...'.",
+        "In the bottom left panel, Paint Ting has a raised eyebrow and Tingker Bell is shrugging with a smile.",
+        "Tingker Bell says 'Yeah, but it makes it easier for you to draw me.'.",
+        "In the bottom right panel, Meta Ting appears and says 'Ah, meta shit.'",
+      ].join(" "),
+    },
+    tags: ["Tingker Bell", "Paint Ting", "Meta Ting"],
+  },
+  {
+    title: "Nope",
+    publishDate: "2021-01-09T22:56:00",
+    image: {
+      src: "/images/paintings/nope.png",
+      alt: [
+        "4 panel comic.",
+        "In the top left panel, Tingker Bell is standing outside a hedge.",
+        "In the top right panel, Tingker Bell is slighting into the hedge.",
+        "In the bottom left panel, Tingker Bell is inside the hedge with only their face and belly visible.",
+        "In the bottom right panel, Tingker Bell is completely immersed in the hedge.",
+      ].join(" "),
+    },
+    tags: ["Tingker Bell"],
+  },
+  {
+    title: "Some Tings",
+    publishDate: "2021-01-09T22:51:00",
+    image: {
+      src: "/images/paintings/some-tings.png",
+      alt: [
+        "4 panel comic.",
+        "In the top left panel with a title 'Miscalculate Ting', Tingker Bell is calculating that 1 + 1 = a window.",
+        "In the top right panel with a title 'Split Ting', Tingker Bell is split into half with a gap inbetween each.",
+        "In the bottom left panel with a title 'Fart Ting', Tingker Bell is cheerful and off the ground like a rocket with dark green 'smoke'.",
+        "In the bottom right panel with a title 'Skywrite Ting', Tingker Bell flying with clouds that spell out 'Skywrite Ting'.",
+      ].join(" "),
+    },
+    tags: ["Miscalculate Ting", "Split Ting", "Fart Ting", "Skywrite Ting"],
+  },
+  {
+    title: "I am Tingker Bell",
+    publishDate: "2021-01-09T22:50:00",
+    image: {
+      src: "/images/paintings/i-am-tingker-bell.png",
+      alt: [
+        "4 panel comic.",
+        "In the top left panel, Tingker Bell's face is at the forefront with a cute smile on a yellow background.",
+        "In the top right panel, Tingker Bell's face is at the forefront with a straight mouth on a blue background.",
+        "In the bottom left panel, Tingker Bell's face is at the forefront with a big smile on a pink background.",
+        "In the bottom right panel, Tingker Bell's face is at the forefront with a shocked expression on a green background.",
+      ].join(" "),
     },
     tags: ["Tingker Bell"],
   },
