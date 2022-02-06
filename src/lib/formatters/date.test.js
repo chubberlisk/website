ca9690ed9a3f5139d2e46ed6acb94f874f9e7eed
Wen Ting Wang <chubberlisk@users.noreply.github.com>
2022-02-06 15:56:29 +0000

    Refactor test names for date formatter

--- src/lib/formatters/date.test.js
+++ src/lib/formatters/date.test.js
@@ -1,9 +1,9 @@
 import { formatDate } from "./date";
 
-it("formats a date", () => {
+it("formats a date like 1st March 2021", () => {
   expect(formatDate("2021-03-01")).toBe("1st March 2021");
 });
 
-it("returns null if unparsable date", () => {
+it("returns null if invalid date", () => {
   expect(formatDate("2021-24-01")).toBeNull();
 });
