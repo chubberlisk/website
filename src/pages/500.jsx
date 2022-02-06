c86e05429eeac9c7ca464dedbf2ed2441fd5b1f4
Wen Ting Wang <chubberlisk@users.noreply.github.com>
2022-02-06 13:26:04 +0000

    Add 500 page

---
+++ src/pages/500.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import Layout from "../components/core/Layout";
+import Section from "../components/core/Section";
+
+export default function Custom404() {
+  return (
+    <Layout title="Internal server error | Wen Ting Wang">
+      <Section minHeight="min-h-screen-90">
+        <div className="container">
+          <h1>🔥 Internal server error</h1>
+          <p>Eh, sorry lah! Something has seriously gone wrong.</p>
+          <p>
+            Try refreshing this page or please{" "}
+            <a
+              href="https://github.com/chubberlisk/website/issues/new"
+              target="_blank"
+              rel="noopener noreferrer"
+              aria-label="Create a GitHub issues for Wen Ting Wang's website"
+            >
+              create an issue on my GitHub repository
+            </a>{" "}
+            if this issue persists.
+          </p>
+        </div>
+      </Section>
+    </Layout>
+  );
+}
