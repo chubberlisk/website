bebd8fb87ec1de2ed74c3e38599eeedac233761f
Wen Ting Wang <chubberlisk@users.noreply.github.com>
2022-06-03 14:21:01 +0100

    Update external links to no longer open in a new tab
    This can be quite disorienting for people, see https://www.w3.org/TR/WCAG20-TECHS/G200.html for more information.

--- src/components/core/ExternalLink.jsx
+++ src/components/core/ExternalLink.jsx
@@ -2,13 +2,7 @@ import React from "react";
 
 export default function ExternalLink({ href, className, ariaLabel, children }) {
   return (
-    <a
-      href={href}
-      target="_blank"
-      rel="noopener noreferrer"
-      aria-label={ariaLabel}
-      className={className}
-    >
+    <a href={href} aria-label={ariaLabel} className={className}>
       {children}
     </a>
   );
