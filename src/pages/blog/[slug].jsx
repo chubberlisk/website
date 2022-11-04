be65698d68af0dc02b1a68dd6542b473c33047d6
Wen Ting Wang <wenting@madetech.com>
2022-11-04 13:11:25 +0000

    Add OpenGraph seo for blog post pages

--- src/pages/blog/[slug].jsx
+++ src/pages/blog/[slug].jsx
@@ -1,17 +1,38 @@
 import React from "react";
+import { NextSeo } from "next-seo";
+import { useRouter } from "next/router";
 import Layout from "../../components/core/Layout";
 import MarkdownGateway from "../../gateways/markdownGateway";
 import GetBlogPostBySlug from "../../useCases/getBlogPostBySlug";
 import GetBlogPosts from "../../useCases/getBlogPosts";
 import BlogPostSection from "../../components/custom/BlogPostSection";
-import { NextSeo } from "next-seo";
 
 export default function Blog({ blogPost }) {
+  const { asPath } = useRouter();
+
   return (
     <>
       <NextSeo
         title={`${blogPost.metadata.title} - Blog`}
         description={blogPost.metadata.summary}
+        openGraph={{
+          title: blogPost.metadata.title,
+          description: blogPost.metadata.summary,
+          url: `https://wentingwang.co.uk${asPath}`,
+          type: "article",
+          article: {
+            publishedTime: blogPost.metadata.publishDate,
+            authors: [],
+            tags: blogPost.metadata.tags,
+          },
+          images: [
+            {
+              url: `https://wentingwang.co.uk${blogPost.metadata.coverImage}`,
+              width: 1000,
+              height: 420,
+            },
+          ],
+        }}
       />
       <Layout>
         <BlogPostSection blogPost={blogPost} />
