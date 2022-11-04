08c13782a6309ee7bb0d45c5de8315004ef6b04e
Wen Ting Wang <wenting@madetech.com>
2022-11-04 12:04:01 +0000

    Update spec pattern for end to end tests

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,7 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     baseUrl: "http://localhost:3001/",
-    specPattern: "test/e2e",
+    specPattern: "test/e2e/**/*.test.js",
     fixturesFolder: "test/e2e/fixtures",
     supportFile: "test/e2e/support/index.js",
   },
