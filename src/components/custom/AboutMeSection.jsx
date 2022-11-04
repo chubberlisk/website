eb480fa4837ee04bcfacfb4e7a9775d50e996c9e
Wen Ting Wang <wenting@madetech.com>
2022-11-04 10:44:42 +0000

    Add Terraform logo component
    - Refactor about me section to use new component.

--- src/components/custom/AboutMeSection.jsx
+++ src/components/custom/AboutMeSection.jsx
@@ -4,6 +4,7 @@ import Section from "../core/Section";
 import ExternalLink from "../core/ExternalLink";
 import Text from "../core/Text";
 import TypeScriptLogo from "../core/icons/TypeScriptLogo";
+import TerraformLogo from "../core/icons/TerraformLogo";
 
 export default function AboutMeSection() {
   return (
@@ -92,34 +93,7 @@ export default function AboutMeSection() {
         </div>
 
         <div className="flex justify-start items-center">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            xmlnsXlink="http://www.w3.org/1999/xlink"
-            aria-hidden="true"
-            role="img"
-            width="2rem"
-            height="2rem"
-            className="mr-3"
-            preserveAspectRatio="xMidYMid meet"
-            viewBox="0 0 256 291"
-          >
-            <rect
-              x="0"
-              y="0"
-              width="256"
-              height="291"
-              fill="none"
-              stroke="none"
-            />
-            <path
-              fill="#4040B2"
-              d="M176.485 188.994L256 143.127V51.25l-79.515 45.963z"
-            />
-            <path
-              d="M88.243 51.249l79.515 45.963v91.782L88.243 143.08M0 91.83l79.515 45.916v-91.83L0 0m88.243 244.994l79.515 45.915v-91.83l-79.515-45.915"
-              fill="#5C4EE5"
-            />
-          </svg>
+          <TerraformLogo />
           <span>Terraform</span>
         </div>
 
