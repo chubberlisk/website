85452a758a788cff62f0c0a79cec73aedfe966dd
Wen Ting Wang <42817036+chubberlisk@users.noreply.github.com>
2023-07-23 13:30:08 +0100

    Rename date formatter to dateTime formmater

--- src/components/core/DateTime.jsx
+++ src/components/core/DateTime.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { formatDate } from "@/formatters/date";
+import { formatDate } from "@/formatters/dateTime";
 
 export default function DateTime({ dateTime }) {
   return <time dateTime={dateTime}>{formatDate(dateTime)}</time>;
