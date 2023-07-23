b48746ff02a53ce4a3469aa0ca610470884c7ecc
Wen Ting Wang <42817036+chubberlisk@users.noreply.github.com>
2023-07-23 12:47:51 +0100

    Move writings page into its own directory

--- pages/writings/index.jsx
+++ pages/writings/index.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-import Layout from "../src/components/core/Layout";
-import GetWritings from "../src/useCases/getWritings";
-import MarkdownGateway from "../src/gateways/markdownGateway";
-import WritingsSection from "../src/components/custom/WritingsSection";
+import Layout from "../../src/components/core/Layout";
+import GetWritings from "../../src/useCases/getWritings";
+import MarkdownGateway from "../../src/gateways/markdownGateway";
+import WritingsSection from "../../src/components/custom/WritingsSection";
 import { NextSeo } from "next-seo";
 
-export default function Writings({ writings }) {
+export default function Index({ writings }) {
   return (
     <>
       <NextSeo
