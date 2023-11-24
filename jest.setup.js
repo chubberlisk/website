8eaeafa289a1bc4a80a2192ac750996f2577d714
Wen Ting Wang <42817036+chubberlisk@users.noreply.github.com>
2023-11-24 14:58:06 +0000

    Add jest-fetch-mock package

--- jest.setup.js
+++ jest.setup.js
@@ -1,5 +1,8 @@
 import "@testing-library/jest-dom";
 import { loadEnvConfig } from "@next/env";
+import fetchMock from "jest-fetch-mock";
+
+fetchMock.enableMocks();
 
 export default async () => {
   const projectDir = process.cwd();
