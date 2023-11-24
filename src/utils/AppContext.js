a2a3a52281bf58c2f4fdfa2829327b6e736d821b
Wen Ting Wang <42817036+chubberlisk@users.noreply.github.com>
2023-11-24 15:16:31 +0000

    Add DatamuseApiGateway and GetTingPuns use case to AppContext

--- src/utils/AppContext.js
+++ src/utils/AppContext.js
@@ -2,13 +2,17 @@ import MarkdownGateway from "@/gateways/MarkdownGateway";
 import GetBlogPosts from "@/useCases/GetBlogPosts";
 import GetWritings from "@/useCases/GetWritings";
 import GetBlogPostBySlug from "@/useCases/GetBlogPostBySlug";
+import DatamuseApiGateway from "@/gateways/DatamuseApiGateway";
+import GetTingPuns from "@/useCases/GetTingPuns";
 
 // Gateways
 export const markdownGateway = new MarkdownGateway({
   contentDirectory: "content",
 });
+export const datamuseApiGateway = new DatamuseApiGateway();
 
 // Use cases
 export const getBlogPostBySlug = new GetBlogPostBySlug(markdownGateway);
 export const getBlogPosts = new GetBlogPosts(markdownGateway);
 export const getWritings = new GetWritings(markdownGateway);
+export const getTingPuns = new GetTingPuns(datamuseApiGateway);
