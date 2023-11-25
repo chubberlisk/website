8c43a87bfec6ca61be9845416479de030d80a534
Wen Ting Wang <42817036+chubberlisk@users.noreply.github.com>
2023-11-25 17:27:18 +0000

    Remove Cypress production config file

--- test/smoke/cypress.config.js
+++ test/smoke/cypress.config.js
@@ -1,6 +1,10 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  browser: {
+    userAgent:
+      "Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)",
+  },
   e2e: {
     baseUrl: "http://localhost:3001/",
     specPattern: "**/*.test.js",
